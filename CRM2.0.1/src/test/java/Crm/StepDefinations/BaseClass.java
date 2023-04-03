7f7555b37b76df74e53df3130e2334388f70cab1
Vinay <bpo-vinay.gaonkar@neogrowth.in>
2023-04-03 13:05:48 +0530

    SecondChanges

---
+++ CRM2.0.1/src/test/java/Crm/StepDefinations/BaseClass.java
@@ -0,0 +1,9 @@
+package Crm.StepDefinations;
+
+import org.apache.logging.log4j.Logger;
+
+public class BaseClass {
+	
+	public static Logger logger;
+
+}
---
+++ CRM2.0.1/src/test/java/Uttiles/WaitUtility.java
@@ -0,0 +1,33 @@
+package Uttiles;
+
+import java.time.Duration;
+
+import org.junit.rules.Timeout;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitUtility {
+	
+	
+	public WebDriver driver;
+
+	/**
+	 * @param driver
+	 */
+	public WaitUtility(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	   public void WaitForElement(WebElement elementaddress,long seconds)
+	   {
+       WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(seconds));
+	   wait.until(ExpectedConditions.visibilityOf(elementaddress));
+	   }    
+ 
+	   public void implcitywait(long sec) {
+		   
+		   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(sec));
+	   }
+}
